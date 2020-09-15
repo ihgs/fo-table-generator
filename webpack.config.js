b6ec0bb6f56b71fe348cd41d8d855bbf3a81316f
ihgs <hgs.dev@gmail.com>
2020-09-15 14:07:26 +0900

    introduce less

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/js/index.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules:[
+      { 
+        test: /\.less$/,
+        loader: ['style-loader', 'css-loader', 'less-loader'],
+      }
+    ]
   }
 };
